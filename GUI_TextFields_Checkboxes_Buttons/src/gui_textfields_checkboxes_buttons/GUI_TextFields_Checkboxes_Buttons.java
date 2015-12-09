3100cba365a25d3cbf4b99743582de22e8c555f4
Mauricio S. Perez <maurime2@gmail.com>
2015-12-09 15:34:20 -0800

    Signed-off-by: Mauricio S. Perez <maurime2@gmail.com>

---
+++ GUI_TextFields_Checkboxes_Buttons/src/gui_textfields_checkboxes_buttons/GUI_TextFields_Checkboxes_Buttons.java
@@ -0,0 +1,11 @@
+//Libraries
+package gui_textfields_checkboxes_buttons;
+
+
+public class GUI_TextFields_Checkboxes_Buttons {
+
+    public static void main(String[] args) {
+         MyFrame frm = new MyFrame();
+    }
+    
+}
