3470bbe5ad78f353d79d1258d04154fdb102150a
Mauricio S. Perez <maurime2@gmail.com>
2016-04-03 22:41:22 -0700

    Signed-off-by: Mauricio S. Perez <maurime2@gmail.com>

---
+++ Java/01. JAVA PROGRAMING TUTORIALS/25-MathClassMethods/src/pkg25/mathclassmethods/MathClassMethods.java
@@ -0,0 +1,23 @@
+//Libraries
+package pkg25.mathclassmethods;
+
+//Class MathClassMethods
+public class MathClassMethods {
+
+    //Main Start
+    public static void main(String[] args) {
+        //Absolute Value
+        System.out.println("Math.abs(-26.7)   = "+Math.abs(-26.7));
+        //round Up
+        System.out.println("Math.ceil(7.4)    = "+Math.ceil(7.4));
+        //Round Down
+        System.out.println("Math.floor(7.4)   = "+Math.floor(7.4));
+        //Maximum
+        System.out.println("Math.max(8.6,5.2) = "+Math.max(8.6,5.2));
+        //Minimum
+        System.out.println("Math.min(8.6,5.2) = "+Math.min(8.6,5.2));
+        //Square Root
+        System.out.println("Math.sqrt(9)      = "+Math.sqrt(9));
+    }//End Main
+    
+}//ENd Class MathClassMethods
